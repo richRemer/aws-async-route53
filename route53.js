05821bb504417297dbe6f6e3b220e19eec993057
Richard Remer <rich.remer@gmail.com>
2021-10-11 14:20:15 -0700

    implement more API calls

--- route53.js
+++ route53.js
@@ -1,20 +1,36 @@
 const paginate = require("aws-async-paginate");
 
 class Route53 {
-    constructor(sdk) {
-        this.sdk = sdk;
-        this.route53 = new sdk.Route53();
-    }
+  constructor(sdk) {
+    this.sdk = sdk;
+    this.route53 = new sdk.Route53();
+  }
 
-    async *listHostedZones() {
-        const fn = params => this.route53.listHostedZones(params);
-        yield* paginate(fn, "HostedZones");
-    }
+  async changeResourceRecordSets({
+    HostedZoneId,
+    ChangeBatch: {Changes=[]},
+    ...params
+  }) {
+    params = {HostedZoneId, ChangeBatch: {Changes}, ...params};
+    return this.route53.changeResourceRecordSets(params).promise();
+  }
 
-    async *listResourceRecordSets(HostedZoneId) {
-        const fn = params => this.route53.listResourceRecordSets(params);
-        yield* paginate(fn, "ResourceRecordSets", {HostedZoneId});
-    }
+  async *listHostedZones() {
+    const fn = params => this.route53.listHostedZones(params);
+    yield* paginate(fn, "HostedZones");
+  }
+
+  async *listHostedZonesByName({
+    ...params
+  }={}) {
+    const fn = opts => this.route53.listHostedZonesByName({...params, ...opts});
+    yield* paginate(fn, "HostedZones");
+  }
+
+  async *listResourceRecordSets(HostedZoneId) {
+    const fn = params => this.route53.listResourceRecordSets(params);
+    yield* paginate(fn, "ResourceRecordSets", {HostedZoneId});
+  }
 }
 
 module.exports = {Route53};
